c38830d319d5e25eaf09d7abce6797c606c3cc60
Eduardo Barrientos <kdoomsday@gmail.com>
2018-07-02 22:58:50 -0400

    Esqueleto de servidor

---
+++ server/src/main/scala/CourierServer.scala
@@ -0,0 +1,29 @@
+import cats.effect._
+import fs2.StreamApp.ExitCode
+import fs2.{ Stream, StreamApp }
+import org.http4s._
+import org.http4s.dsl.io._
+import org.http4s.server.blaze._
+import scala.concurrent.ExecutionContext.Implicits.global
+
+object CourierServer extends StreamApp[IO] {
+
+  val helloService = HttpService[IO] {
+    case GET -> Root / "hello" / name =>
+      Ok(s"Hello, $name!")
+  }
+
+  def shutdownService(requestShutdown: IO[Unit]) = HttpService[IO] {
+    case GET -> Root / "shutdown" =>
+      requestShutdown.unsafeRunSync()
+      Ok()
+  }
+
+  override def stream(args: List[String], requestShutdown: IO[Unit]): Stream[IO, ExitCode] = {
+    BlazeBuilder[IO]
+      .bindHttp(9000, "localhost")
+      .mountService( helloService )
+      .mountService( shutdownService(requestShutdown) )
+      .serve
+  }
+}
--- server/src/main/scala/courier/auth/Autenticador.scala
+++ server/src/main/scala/courier/auth/Autenticador.scala
@@ -2,6 +2,7 @@ package courier.auth
 
 /** Maneja la autenticación de nodos */
 trait Autenticador {
+
   /** Autenticar un nodo
     * @param creds Las credenciales con las que se intenta autenticar
     * @return El error de autenticación si falló. Si fue exitosa el token de
